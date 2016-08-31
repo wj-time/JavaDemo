c2280e27b09dcdaebc0b5113eb2d17e7208254ac
wujing2015 <413249040@qq.com>
2016-08-31 15:54:46 +0800

    Initial commit

---
+++ HelloWorld/src/com/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by 41324 on 2016/8/31.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+    }
+}
---
+++ HelloWorld/src/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test; /**
+ * Created by 41324 on 2016/8/31.
+ */
+
+/**
+ * 测试自定义注解
+ *
+ * @author 41324
+ * @create 2016-08-31 12:24
+ **/
+public class Test {
+}
